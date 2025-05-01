2a6ccbc57ce23820667c9cf510e13f06ce0f3d01
Pravin <pravin.nimodiya@ideas.com>
2025-05-01 12:07:54 +0530

    frontend and default sql

---
+++ src/main/resources/db/init.sql
@@ -0,0 +1,15 @@
+-- public.confluence_vector definition
+
+-- Drop table
+
+-- DROP TABLE public.confluence_vector;
+
+CREATE DATABASE vector_db;
+\c vector_db;
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE TABLE IF NOT EXISTS public.confluence_vector (
+                                          id serial4 NOT NULL,
+                                          metadata text NULL,
+                                          vectors public.vector NULL,
+                                          CONSTRAINT confluence_vector_pkey PRIMARY KEY (id)
+);
