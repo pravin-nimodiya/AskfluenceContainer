1c1749ffd3eee18bda7da3d60c54e27e5fdca6d6
Pravin <pravin.nimodiya@ideas.com>
2025-05-14 14:38:32 +0530

    use session token false

--- src/main/java/com/ideas/askfluence/config/Connections.java
+++ src/main/java/com/ideas/askfluence/config/Connections.java
@@ -14,7 +14,7 @@ public class Connections {
     private String accessKey;
     @Value("${aws.s3.secret-key:3HUoqvaQiClRymC40gxrKMmQbsJ6hs0sJvPdES1I}")
     private String secretKey;
-    @Value("${aws.use.session.token:true}")
+    @Value("${aws.use.session.token:false}")
     private Boolean useSessionToken;
 
     @Value("${aws.session.token:IQoJb3JpZ2luX2VjECwaCXVzLWVhc3QtMSJGMEQCIHA1RqqmMQYF5tYy0gKWG24UHPEdcst1IhAxr1bZ6vBuAiBHdClMOqcaqbwNuyUb5g0bP7R0TEHFqkQRWtRMHokB+CqaAwil//////////8BEAIaDDExNDI0NDgxNjQwNyIM/+vERc/ITP9C+i4bKu4CJsb4Et/W4e8DljmxgHc/I8kr8+QEIYhvGNsCGc3s6CaWmmV+fxW/BBT8T+Zx1aeBx8uWXBMEMtKgOZLGJ9z04FBNZ9J7ggf4F6+eJNVKjo9YcbOvhrz6y6QlLlX7excrCOCGzDHMkmOyv6YaC56evx9EXRvm/nfbHR1R2xlfnDwYeyh0YK0Gr5d5bnqCdmcsDsNzNVl2exdiAyp3skCxp9LusIL8UygP8h1N/HqJtfvQVwK4daVsdp7lk/R9hFNUndJLJhepe2cle0ou2pDcZ/gUigYwMyEdLYC4mFRapd7IKh54kOMkwc4v04k6ptHRK2L9c/x3apo1Rd49XCtWnJ6v1tV5oKEwhE4+n3XifN79NMDPbPq9z64BBC7PsBGaEIZkR1URUs1ihSk9w+q852WttLzS/0R4f/r7/Y7JYlGMM6IDEo1NQrVjSt8hg17qC6SI5xz4YgF72uk1oXogIXMnxNoYuOlhiPN+J7/OMILU3r8GOqcB6PKq9pq+yOaEsKL6DuReb81/Cu04/LzmcI09RWnQRGWZ8A+JC/QnZppqFOzgKHx/wbZKeNhgdqOrZAhdClHJ5iXtS5ogmvQX9kDBRm06v3rX4MVF1HQRRijiCg424gp8UhtAFk4T8rgRhPLLFqC2vs4iBv0AuTnaFXBllU4qF5Of+Bd/37vPL+scJNBOzy+KeTM8FpCj+OicDfWXjo87dvyDNBgsGlY=}")
